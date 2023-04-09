e5288465c0415c4f0c969b5ed15c17b5f76977e4
mircomellara <mirco.mellara@gmail.com>
2023-04-09 18:47:29 +0200

    Tag added + Alert when discarding changes + Added comments on code
    Added in the bottom-right corner of the "Today's note" Card a simple text that tells the current day to the user.
    Now leaving the page with discarded changes will alert the user with a pop-up.
    Added comments on the MyDiaryActivity.java code, to briefly describe what everything should do.

--- app/src/main/java/com/example/feelingfinder/Diary/MyDiaryActivity.java
+++ app/src/main/java/com/example/feelingfinder/Diary/MyDiaryActivity.java
@@ -1,20 +1,22 @@
 package com.example.feelingfinder.Diary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.feelingfinder.FeelingFinder;
-import com.example.feelingfinder.NotificationActivity;
 import com.example.feelingfinder.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.Map;
 
 public class MyDiaryActivity extends AppCompatActivity {
@@ -24,49 +26,69 @@ public class MyDiaryActivity extends AppCompatActivity {
             todayDate.getMonthValue() + "-" +
             todayDate.getYear();
 
+    private String infoTodaysDateString = todayDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US) + " " + todaysDateString;
+    private String todaysNote = "";
+    private String savedNote = "";
+
+    private EditText todaysNoteRaw;
+
+    private SharedPreferences data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_diary);
 
-        SharedPreferences data = Data.getInstance();
+        // Finds the "tag" in the bottom-right of the card and sets the current day info in it
+        TextView dateTag = findViewById(R.id.dateTag);
+        dateTag.setText(infoTodaysDateString);
 
-        String savedNote = "";
-        System.out.println(todaysDateString);
+        // Retrieve the data instance, in order to be able to edit it and save/edit today's note in
+        // the storage
+        data = Data.getInstance();
 
+        // If today's note is empty, prints it in the log (DEBUG ONLY)
+        // Otherwise saves in the variable "savedNote" the current day's note.
         if (data.getString(todaysDateString, "").isEmpty()){
             System.out.println("No data today");
-            //TODO: popup
         }
         else{
             savedNote = data.getString(todaysDateString, "");
         }
+
+        // Just retrieves some things
         SharedPreferences.Editor editor = Data.getEditor();
-
         Button saveButton = findViewById(R.id.saveButton);
-        EditText todaysNoteRaw = findViewById(R.id.todaysNote);
+        todaysNoteRaw = findViewById(R.id.todaysNote);
+        FloatingActionButton backButton = findViewById(R.id.backButton);
+        Button yest = findViewById(R.id.previousDaysButton);
 
+        // If savedNote isn't empty, it will be displayed in the note
         if (!savedNote.isEmpty()){
             todaysNoteRaw.setText(savedNote);
         }
+
+        // When clicking the save button, writes in the storage today's note
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String todaysNote = todaysNoteRaw.getText().toString();
+                todaysNote = todaysNoteRaw.getText().toString();
                 editor.putString(todaysDateString, todaysNote);
                 editor.apply();
             }
         });
 
-        FloatingActionButton backButton = findViewById(R.id.backButton);
+        // When clicking the back button, goes to the Home Activity
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(FeelingFinder.getAppContext(), NotificationActivity.class));
+                MyDiaryActivity.this.onBackPressed();
             }
         });
 
-        Button yest = findViewById(R.id.previousDaysButton);
+        // ### DEBUG ONLY ###
+        // Clicking the previous days button prints in the log/runner tabs of android studio
+        // a list of all the notes which are not empty/invalid, from all days.
         yest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -96,6 +118,34 @@ public class MyDiaryActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+    }
+
+    // Clicking the back button of the phone, will check if the note has been edited or not.
+    // If so, it will alert the user to remember him of saving
+    @Override
+    public void onBackPressed() {
+        savedNote = data.getString(todaysDateString, "");
+        if (!savedNote.equals(todaysNoteRaw.getText().toString())){
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Discard changes?")
+                    .setMessage("Are you sure you want to go back?\n" +
+                            "All work done has NOT been saved yet!")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
+        else{
+            finish();
+        }
     }
 
 
