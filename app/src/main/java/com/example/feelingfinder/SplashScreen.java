d6ddc5a9179862cf61b267367c6212f344b67b93
Filipa Rodrigues <a21280298@isec.pt>
2023-04-12 19:20:43 +0200

    UI
    Icon Change and Splash Screnn

---
+++ app/src/main/java/com/example/feelingfinder/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.feelingfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 2000);
+
+
+    }
+
+}
