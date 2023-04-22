71f49c31b3108a3e83c43a28dd3f50bcab1bf5d9
mircomellara <mirco.mellara@gmail.com>
2023-04-22 17:40:35 +0200

    Check/Uncheck goals linked to DB and works properly

--- app/src/main/java/com/example/feelingfinder/Goals/GoalsAdapter.java
+++ app/src/main/java/com/example/feelingfinder/Goals/GoalsAdapter.java
@@ -4,11 +4,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.feelingfinder.Database.AppDatabase;
+import com.example.feelingfinder.Database.Database;
 import com.example.feelingfinder.Database.Goal;
+import com.example.feelingfinder.Database.GoalsDAO;
 import com.example.feelingfinder.R;
 
 import java.util.List;
@@ -30,7 +34,7 @@ public class GoalsAdapter extends RecyclerView.Adapter<GoalsAdapter.ViewHolder>{
             checkBox = (CheckBox) view.findViewById(R.id.checkBoxGoal);
         }
 
-        public CheckBox getTextView() {
+        public CheckBox getCheckBoxView() {
             return checkBox;
         }
     }
@@ -63,7 +67,35 @@ public class GoalsAdapter extends RecyclerView.Adapter<GoalsAdapter.ViewHolder>{
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         String content = goalList.get(position).description;
-        viewHolder.getTextView().setText(content);
+        Boolean checked = goalList.get(position).status;
+        viewHolder.getCheckBoxView().setText(content);
+        viewHolder.getCheckBoxView().setChecked(checked);
+
+        // Set the listener for the checkbox
+        viewHolder.getCheckBoxView().setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                // Retrieve the Database instance
+                AppDatabase db = Database.getAppDatabase();
+                // Get access to the goals query
+                GoalsDAO gDao = db.goalsDAO();
+                // Queries the goal
+                Goal goal = gDao.getGoal(goalList.get(viewHolder.getAdapterPosition()).id);
+                if(isChecked){
+                    System.out.println("Checked goal #" + goalList.get(viewHolder.getAdapterPosition()).id + ": " + goal.description);
+                    // Updates the goal
+                    goal.status = true;
+                    gDao.updateGoal(goal);
+                }
+                else {
+                    System.out.println("Unchecked goal #" + + goalList.get(viewHolder.getAdapterPosition()).id + ": " + goal.description);
+                    // Updates the goal
+                    goal.status = false;
+                    gDao.updateGoal(goal);
+                }
+            }
+        });
+
     }
 
 
