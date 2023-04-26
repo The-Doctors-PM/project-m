d18b8d53bca248384183389bfccbe344b685f035
Filipa Rodrigues <a21280298@isec.pt>
2023-04-26 10:54:03 +0200

    UI + Quiz
    Changes on the UI plus, one new activity

--- app/src/main/java/com/example/feelingfinder/MainActivity.java
+++ app/src/main/java/com/example/feelingfinder/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.feelingfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.feelingfinder.databinding.ActivityMainBinding;
+import com.ramotion.circlemenu.CircleMenuView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +23,71 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        TextView welcomeString;
-        welcomeString = binding.textView;
-        welcomeString.setText("Welcome to \"Feeling Finder\"");
-        welcomeString.setTextSize(24);
+        final CircleMenuView menu = findViewById(R.id.circle_menu);
+        menu.setEventListener(new CircleMenuView.EventListener(){
+            @Override
+            public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuOpenAnimationStart");
+            }
 
+            @Override
+            public void onMenuOpenAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuOpeneAnimationEnd");
+            }
+
+            @Override
+            public void onMenuCloseAnimationStart(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuCloseAnimationStart");
+            }
+
+            @Override
+            public void onMenuCloseAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuCloseAnimationEnd");
+            }
+
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int buttonIndex) {
+                Log.d("D","onButtonClickAnimationStart|index: "+buttonIndex);
+            }
+
+            @Override
+            public void onButtonClickAnimationEnd(@NonNull CircleMenuView view, int buttonIndex) {
+                Log.d("D","onButtonClickAnimationEnd|index: "+buttonIndex);
+
+                // Start a new activity when a button is clicked
+                Intent intent;
+                switch (buttonIndex) {
+                    case 0:
+                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        MainActivity.this.startActivity(intent);
+                        break;
+
+                    case 1:
+                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 4:
+                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+
+            @Override
+            public boolean onButtonLongClick(@NonNull CircleMenuView view, int buttonIndex) {
+                Log.d("D","onButtonLongClick|index: "+buttonIndex);
+                return true;
+            }
+
+
+        });
     }
-
 }
