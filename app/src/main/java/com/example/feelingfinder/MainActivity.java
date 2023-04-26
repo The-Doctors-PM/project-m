5a64ad3b9812d338585c3603b97fa278a5fe5876
mircomellara <mirco.mellara@gmail.com>
2023-04-26 13:10:08 +0200

    Merge finalization and conflict resolutions

--- app/src/main/java/com/example/feelingfinder/MainActivity.java
+++ app/src/main/java/com/example/feelingfinder/MainActivity.java
@@ -1,91 +1,87 @@
 package com.example.feelingfinder;
 
-<<<<<<< HEAD:app/src/main/java/com/example/feelingfinder/NotificationTappedActivity.java
 import androidx.appcompat.app.AppCompatActivity;
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
 
+import com.example.feelingfinder.Diary.MyDiaryActivity;
+import com.example.feelingfinder.Goals.GoalsActivity;
+import com.example.feelingfinder.Profile.ProfilePlaceholderActivity;
+import com.example.feelingfinder.Utility.FeelingFinder;
 import com.example.feelingfinder.databinding.ActivityMainBinding;
 import com.ramotion.circlemenu.CircleMenuView;
->>>>>>> FirstDemoUI:app/src/main/java/com/example/feelingfinder/MainActivity.java
 
-import android.os.Bundle;
-
-// Activity that appears when you click the notification
-public class NotificationTappedActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/example/feelingfinder/NotificationTappedActivity.java
-        setContentView(R.layout.activity_notification_tapped);
-    }
-}
-=======
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ViewBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         final CircleMenuView menu = findViewById(R.id.circle_menu);
-        menu.setEventListener(new CircleMenuView.EventListener(){
+        menu.setEventListener(new CircleMenuView.EventListener() {
             @Override
             public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
-                Log.d("D","onMenuOpenAnimationStart");
+                Log.d("D", "onMenuOpenAnimationStart");
             }
 
             @Override
             public void onMenuOpenAnimationEnd(@NonNull CircleMenuView view) {
-                Log.d("D","onMenuOpeneAnimationEnd");
+                Log.d("D", "onMenuOpeneAnimationEnd");
             }
 
             @Override
             public void onMenuCloseAnimationStart(@NonNull CircleMenuView view) {
-                Log.d("D","onMenuCloseAnimationStart");
+                Log.d("D", "onMenuCloseAnimationStart");
             }
 
             @Override
             public void onMenuCloseAnimationEnd(@NonNull CircleMenuView view) {
-                Log.d("D","onMenuCloseAnimationEnd");
+                Log.d("D", "onMenuCloseAnimationEnd");
             }
 
             @Override
             public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int buttonIndex) {
-                Log.d("D","onButtonClickAnimationStart|index: "+buttonIndex);
+                Log.d("D", "onButtonClickAnimationStart|index: " + buttonIndex);
             }
 
             @Override
             public void onButtonClickAnimationEnd(@NonNull CircleMenuView view, int buttonIndex) {
-                Log.d("D","onButtonClickAnimationEnd|index: "+buttonIndex);
+                Log.d("D", "onButtonClickAnimationEnd|index: " + buttonIndex);
 
                 // Start a new activity when a button is clicked
                 Intent intent;
                 switch (buttonIndex) {
                     case 0:
-                        intent = new Intent(MainActivity.this, QuizActivity.class);
-                        MainActivity.this.startActivity(intent);
+                        intent = new Intent(FeelingFinder.getAppContext(), ProfilePlaceholderActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        FeelingFinder.getAppContext().startActivity(intent);
                         break;
-
                     case 1:
-                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        intent = new Intent(FeelingFinder.getAppContext(), MyDiaryActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                         break;
                     case 2:
-                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        intent = new Intent(FeelingFinder.getAppContext(), GoalsActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                         break;
                     case 3:
-                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        intent = new Intent(FeelingFinder.getAppContext(), QuizActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                         break;
                     case 4:
-                        intent = new Intent(MainActivity.this, QuizActivity.class);
+                        intent = new Intent(FeelingFinder.getAppContext(), QuizActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                         break;
                 }
@@ -93,7 +89,7 @@ public class NotificationTappedActivity extends AppCompatActivity {
 
             @Override
             public boolean onButtonLongClick(@NonNull CircleMenuView view, int buttonIndex) {
-                Log.d("D","onButtonLongClick|index: "+buttonIndex);
+                Log.d("D", "onButtonLongClick|index: " + buttonIndex);
                 return true;
             }
 
@@ -101,4 +97,3 @@ public class NotificationTappedActivity extends AppCompatActivity {
         });
     }
 }
->>>>>>> FirstDemoUI:app/src/main/java/com/example/feelingfinder/MainActivity.java
--- app/src/main/java/com/example/feelingfinder/SplashScreen.java
+++ app/src/main/java/com/example/feelingfinder/SplashScreen.java
@@ -1,12 +1,16 @@
 package com.example.feelingfinder;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.feelingfinder.Database.Database;
+import com.example.feelingfinder.Utility.FeelingFinder;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -21,9 +25,20 @@ public class SplashScreen extends AppCompatActivity {
 
             @Override
             public void run() {
+                // If Device is Android Oreo or more recent, creates the notification channels
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(
+                            FeelingFinder.getChannelId(),
+                            "Feeling Finder Channel",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+                    NotificationManager manager = getSystemService(NotificationManager.class);
+                    manager.createNotificationChannel(channel);
+                }
+
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, HomepageActivity.class);
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
 
                 // close this activity
