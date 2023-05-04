bd377e082082c89cf85b3e27447636631a8ac37e
LeonardoB25 <84146150+LeonardoB25@users.noreply.github.com>
2023-05-04 11:59:01 +0200

    First settings tuning, mostly on xml
    Will link backend to front-end better in the next push

--- app/src/main/java/com/example/feelingfinder/SettingsActivity/SettingsActivity.java
+++ app/src/main/java/com/example/feelingfinder/SettingsActivity/SettingsActivity.java
@@ -1,7 +1,13 @@
 package com.example.feelingfinder.SettingsActivity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
 }
