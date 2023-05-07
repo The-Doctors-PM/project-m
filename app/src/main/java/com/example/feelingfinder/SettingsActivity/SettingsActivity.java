bbecff22af19c744452fa35ba1e90773c134629f
LeonardoB25 <84146150+LeonardoB25@users.noreply.github.com>
2023-05-07 16:36:48 +0200

    Linked front-end buttons with backend stubs

--- app/src/main/java/com/example/feelingfinder/SettingsActivity/SettingsActivity.java
+++ app/src/main/java/com/example/feelingfinder/SettingsActivity/SettingsActivity.java
@@ -1,13 +1,25 @@
 package com.example.feelingfinder.SettingsActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.feelingfinder.R;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
+        // button which will let the user edit the mood tracker's notification time
+        Button moodTrackerEditButton = findViewById(R.id.moodTrackerEditTimeButton);
+
+        // button which lets the user upload his/her own profile picture or avatar
+        Button uploadProfilePicture = findViewById(R.id.profilePictureUploadButton);
+
+        // button which lets the user export his/her diary
+        Button dataExportButton = findViewById(R.id.dataExportButton);
     }
 }
